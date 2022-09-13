55527d4c47b548feaa96ee5f87367601f0120772
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-13 16:50:37 -0700

    Add 0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,10 @@
 /**
  * main - Entry point
  *
- *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-
-	for (n = 0 ; n < 10 ; n++)
-	{
-		putchar(n + '0');
-	}
-	putchar('\n');
+	printf("_putchar");
 	return (0);
 }
