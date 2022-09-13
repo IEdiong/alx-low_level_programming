87f34f8d30762be06f82735a27a05078fe051816
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-13 15:59:15 -0700

    Hotfix for 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,17 +3,14 @@
 /**
  * main - Entry point
  *
- *
- * Return: Always 0 (Sucess)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
-	
-	for (n = 0 ; n < 10 ; n++)
-	{
-		printf("%d", n);
-	}
+
+	for (n = 0 ; n <= 9 ; n++)
+		putchar(n);
 	putchar('\n');
 	return (0);
 }
