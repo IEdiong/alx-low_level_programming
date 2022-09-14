9360ee3355a6dce2437b83d5f4a3416aad55cbb7
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-13 21:55:59 -0700

    Add Hotfix 11-print_to_98 v.3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 
 	if (n == 98)
 	{
-		printf(98);
+		printf("98");
 	}
 	else if (n < 98)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			printf(i);
+			printf("%d", i);
 			if (i < 98)
 				printf(", ");
 		}
@@ -26,10 +26,10 @@ void print_to_98(int n)
 	{
 		for (i = n ; i >= 98 ; i--)
 		{
-			printf(i);
+			printf("%d", i);
 			if (i > 98)
 				printf(", ");
 		}
 	}
-	printf('\n');
+	printf("\n");
 }
