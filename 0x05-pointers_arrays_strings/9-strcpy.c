f707ddd868d905db864aa544509cc077bf23ea91
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-19 21:56:40 -0700

    add 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _strcpy - copies string pointed to by a pointer
+ * @dest: destination
+ * @src: source
+ *
+ * Return: the value of the destination pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int idx = 0;
+
+	while (*src != '\0')
+	{
+		dest[idx] = *src;
+		idx++;
+		src++;
+	}
+	return (dest);
+}
