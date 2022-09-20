5274dcbd3046edb2bdc5899a0c3cd40cebfac1b4
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-19 20:01:40 -0700

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every two character of a string, skipping one
+ * @str: string parameter
+ */
+
+void puts2(char *str);
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "0123456789";
+    puts2(str);
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * puts2 - prints every two character of a string, skipping one
+ * @str: string parameter
+ */
+
+void puts2(char *str)
+{
+	int i, n;
+
+	n = 0;
+	while (str[n] != '\0')
+		n++;
+	for (i = 0; i < n - 1; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
