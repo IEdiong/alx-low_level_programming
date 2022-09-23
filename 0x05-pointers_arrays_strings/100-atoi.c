c298e4a9db7620f2b74836db2f373bf1b78b33b6
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-23 02:32:38 -0700

    add Hotfix v.2 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -16,16 +16,17 @@ int _atoi(char *s)
 
 		while (*(s + i) >= '0' && *(s + i) <= '9')
 		{
+			if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > 7))
+			{
+				if (sign == 1)
+					return (INT_MAX);
+				else
+					return (INT_MIN);
+			}
 			res = res * 10 + (s[i++] - '0');
+
 			if (!(s[i] >= '0' && s[i] <= '9'))
 			{
-				if (res > INT_MAX / 10 || (res == INT_MAX / 10 && s[i] - '0' > 7))
-				{
-					if (sign == 1)
-						return (INT_MAX);
-					else
-						return (INT_MIN);
-				}
 				return (sign * res);
 			}
 		}
