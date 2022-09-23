a4e1749283b8c8d178b35076c7961b552a78c70e
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-23 14:07:20 -0700

    Add 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+/**
+ * rot13 - encodes a string using the rot13
+ * @s: string to encode
+ *
+ * Return: pointer to the encoded string
+ */
+
+char *rot13(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		while ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+		{
+			*(s + i) =  s[i] + 13;
+			i++;
+		}
+		if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			*(s + i) = s[i] - 13;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,8 @@
+/**
+ * leet - encodes a string
+ * @s: string to be encoded
+ *
+ * Return: pointer to the encoded string
+ */
+
+char *leet(char *s);
