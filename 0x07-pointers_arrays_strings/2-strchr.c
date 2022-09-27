2fa1399cd5b3609617c09cf4731c738faa70a9af
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-27 10:13:57 -0700

    Add 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchr - locates a char in a string
+ * @s: string to be parsed
+ * @c: character to be searched for
+ *
+ * Return: pointer to the first occurence of the character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	while (s)
+		if (*(s + i) == c)
+			return ((s + i));
+		i++;
+	return (NULL);
+}
