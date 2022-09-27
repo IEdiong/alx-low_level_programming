c209213a0d9e90fc8c8b10561cc6f4192a7e253f
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-26 23:21:14 -0700

    Add init for project 0x07

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -2,6 +2,16 @@
 #include "main.h"
 
 /**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
+
+/**
  * main - prints the largest of 3 integers
  * Return: 0
  */
@@ -11,8 +21,8 @@ int main(void)
 	int a, b, c;
 	int largest;
 
-	a = -972;
-	b = 98;
+	a = 972;
+	b = -98;
 	c = 0;
 
 	largest = largest_number(a, b, c);
