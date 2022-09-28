1100a62097471e948228c0a08da5d02723ef1199
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-27 17:44:07 -0700

    Add 100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a string
+ * @s: double pointer to source
+ * @to: string to copy to
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
