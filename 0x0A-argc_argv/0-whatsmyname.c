f992f90882d270f4500aacab723ee9dc6db6eded
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2022-09-30 10:41:31 -0700

    Add Hotfix v.2.4

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(int argc __attribute((_unused__)), char *argv[])
+int main(int __attribute((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
