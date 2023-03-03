d22517e4e6cc2439479d38161888504da746246c
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-03 08:43:15 -0800

    add code for leet function

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,4 +5,23 @@
  * Return: pointer to the encoded string
  */
 
-char *leet(char *s);
+char *leet(char *s)
+{
+	char *l = ['a', 'e', 'o', 't', 'l'];
+	char *u = ['A', 'E', 'O', 'T', 'L'];
+	int *n = [4, 3, 0, 7, 1];
+	int p = 0;
+	int i;
+
+	while (s[p] != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (s[p] == l[i] || s[p] == u[i])
+				s[p] = n[i];
+		}
+		p++;
+	}
+
+	return (s);
+}
