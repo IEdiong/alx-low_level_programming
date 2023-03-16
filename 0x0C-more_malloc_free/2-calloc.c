2d160213b8a8e7c59394a7bfd4d9c839a50d9307
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-16 14:49:39 -0700

    Fix code for calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,8 +22,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
-		*(arr + i * size) = 0;
+	for (i = 0; i < nmemb * size; i++)
+		*(arr + i) = 0;
 
 	return (arr);
 }
