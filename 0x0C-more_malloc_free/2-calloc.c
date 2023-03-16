549720113d4260e265ca53f0390d27d8f2a460fa
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-16 14:47:34 -0700

    Add code for calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element in the array
+ *
+ * Return: pointer to the allocated memory.
+ * Otherwise NULL if nmemb or size is 0 and malloc fails.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		*(arr + i * size) = 0;
+
+	return (arr);
+}
