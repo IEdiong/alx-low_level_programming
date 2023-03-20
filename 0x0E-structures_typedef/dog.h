bd3373c3879585453d5d0845ae84351caaa329bb
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-20 09:16:14 -0700

    Add struct for dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog - A dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
