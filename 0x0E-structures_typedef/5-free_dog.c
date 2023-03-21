1faeb775b2248e5faaa65c5faf13a71305a6ac1a
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-20 20:22:06 -0700

    Add free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - free up memory allocated to dog
+ * @d: address of dog in memory
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->age);
+	free(d->owner);
+	free(d);
+}
