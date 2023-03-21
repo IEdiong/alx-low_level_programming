a4665f7afdfcd41dc83271073fe938294ce72799
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-20 19:47:49 -0700

    Add code to store a copy of name and owner

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,14 +13,18 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *n, *o;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	d->name = name;
+	n = name;
+	o = owner;
+
+	d->name = n;
 	d->age = age;
-	d->owner = owner;
+	d->owner = o;
 
 	return (d);
 }
