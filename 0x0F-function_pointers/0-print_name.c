3fadebb2adb28dacd046c5ee49d19c2a94abbbf3
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-22 14:54:04 -0700

    Task-0 first trial

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: string to be printed
+ * @f: callback function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
