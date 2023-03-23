517f9643b5387a434463166f9d0bdc76f72082a2
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-23 12:37:34 -0700

    Task 1 second trial

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (i != n - 1)
 		{
-			if (*separator != 0)
+			if (separator != NULL)
 				printf("%s", separator);
 		}
 	}
