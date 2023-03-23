cfa281a0cfb35e69692a714e7b96e1a78effe6df
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-23 14:45:02 -0700

    Task - 3 sixth trial

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -7,39 +7,43 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
-	char *str, *sep;
-	int i;
+	int i = 0;
+	char *str, *sep = "";
 
-	va_start(args, format);
-	i = 0;
-	sep = "";
-	while ((format[i] != '\0') && (format != NULL))
+	va_list list;
+
+	va_start(list, format);
+
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				printf("%s%c", sep, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (str == NULL)
-					str = "(nil)";
-				printf("%s%s", sep, str);
-				break;
-			default:
-				i++;
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		sep = ", ";
-		i++;
 	}
+
 	printf("\n");
-	va_end(args);
+	va_end(list);
 }
