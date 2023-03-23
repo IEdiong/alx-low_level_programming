7abf2f121b2693eb6e215c235d838b9f08005efc
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-23 13:09:28 -0700

    Task - 2 second trial

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(args, char *);
-		printf("%s", str);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
 		if ((separator != NULL) && (i != n - 1))
 			printf("%s", separator);
 	}
