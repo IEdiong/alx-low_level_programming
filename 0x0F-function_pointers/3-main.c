f43c321abedc3e98a2551f41e1aa886d0a507c88
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-22 18:54:49 -0700

    Task-3 first trial

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * main - performs simple arthmetic operations
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0 on success. Otherwise 98 if number of
+ *         arguments is wrong, 99 if the operator is
+ *         invalid or 100 if user tries to divide (/ or %)
+ *         by 0.
+ */
+int main(int argc, char *argv[])
+{
+	int arg1, arg2;
+	int (* opr)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	opr = get_op_func(argv[2]);
+
+	if (!opr)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	arg1 = atoi(argv[1]);
+	arg2 = atoi(argv[3]);
+	if ((!strcmp(argv[2], "/") || !strcmp(argv[2], "%")) && (arg2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", opr(arg1, arg2));
+
+	return (EXIT_SUCCESS);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - sums up two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The sum of two numbers.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract a number from another
+ * @a: number to subtract from
+ * @b: number to subtract
+ *
+ * Return: The difference of two numbers.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: The product of two numbers.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide a number by another
+ * @a: number to divide
+ * @b: number to divide with
+ *
+ * Return: The result of dividing a by b.
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulos of two numbers
+ * @a: number to divide
+ * @b: number to divide with
+ *
+ * Return: The modulos of two numbers.
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
