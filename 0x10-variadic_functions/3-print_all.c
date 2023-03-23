19d485b8ff8acbed86fac5baed4f4a462f9b6d11
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-23 14:42:07 -0700

    Task - 3 fifth trial

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char * const format, ...)
 	va_start(args, format);
 	i = 0;
 	sep = "";
-	while (format[i] != '\0')
+	while ((format[i] != '\0') && (format != NULL))
 	{
 		switch (format[i])
 		{
