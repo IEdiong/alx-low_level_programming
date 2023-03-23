ce210aee7408e590d047a3c5223a16c6fa4fb2a1
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-03-23 13:59:27 -0700

    Task - 3 second trial

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -32,9 +32,8 @@ void print_all(const char * const format, ...)
 			case 's':
 				str = va_arg(args, char *);
 				if (str == NULL)
-					printf("(nil)");
-				else
-					printf("%s", str);
+					str = "(nil)";
+				printf("%s", str);
 				break;
 		}
 		if (format[i + 1] != '\0' && (num_chars + num_ints + num_floats) > 0)
