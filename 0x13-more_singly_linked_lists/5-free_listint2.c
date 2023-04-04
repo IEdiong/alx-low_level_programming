7fad21fae448a7153ed01e86253d0e850c53019b
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-04 07:23:35 -0700

    Task-5 first trial done

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees up a listint_t list
+ * @head: a double pointer to the head of the listint_t list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *prev;
+
+	while (*head != NULL)
+	{
+		prev = *head;
+		*head = (*head)->next;
+		free(prev);
+	}
+	*head = NULL;
+}
