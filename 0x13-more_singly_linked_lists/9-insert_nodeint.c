7518344ef1adc91984a125b536c9a270c019f9ae
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-04 16:21:52 -0700

    Task-9 ninth trial done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,7 +30,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 
-	for (i = 0; curr_node->next != NULL && i < idx; i++)
+	for (i = 0; curr_node != NULL && i < idx; i++)
 	{
 		if (i == idx - 1)
 		{
