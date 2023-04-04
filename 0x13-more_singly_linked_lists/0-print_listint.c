ee6f903c857394a8987e807a57a0f435ae4050bf
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-04 04:26:57 -0700

    Task-0 first trial done

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: head of the listint_t list
+ *
+ * Return: Number of nodes.
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
