a350035be1c6b0627209e5d1fb0c507c1d40a46a
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-04 15:38:11 -0700

    Task-9 fifth trial done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = (*head)->next;
 	}
 
-	if (*head == NULL)
+	if (head == NULL)
 	{
 		*head = dummy_node;
 		return (NULL);
