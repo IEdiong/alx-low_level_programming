8abe90f3acfbdacfd4ae2e66f91bc0d305b3eca5
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-04 15:49:57 -0700

    Task-9 seventh trial done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,8 +17,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *dummy_node;
 	unsigned int i;
 
-	if (head == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL || *head == NULL)
 		return (NULL);
+
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
 	dummy_node = *head;
 	for (i = 0; i != idx; i++)
@@ -32,14 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = dummy_node;
 		return (NULL);
 	}
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		*head = dummy_node;
-		return (NULL);
-	}
-
 	prev->next = new_node;
 	new_node->n = n;
 	new_node->next = *head;
