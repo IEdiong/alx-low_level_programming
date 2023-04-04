f2018989b4c2fb16e856a4bc9ecec901e4dfb233
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-04 15:21:27 -0700

    Task-9 second trial done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *dummy_node;
 	unsigned int i;
 
-	if (*head == NULL)
+	if (head == NULL)
 		return (NULL);
 
 	dummy_node = *head;
