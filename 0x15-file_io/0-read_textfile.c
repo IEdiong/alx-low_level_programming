2b99375a38ec6192a43d3153e32842aea0cd1f5f
Ediongsenyene Inokotong <ediongi6@hotmail.com>
2023-04-10 16:11:20 -0700

    Task-0 third trial done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,31 +11,38 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	char *buff;
-	ssize_t byte, r_byte;
+	ssize_t w_byte, r_byte;
 
 	if (!filename)
 		return (0);
 
 	/* open the file in read/write mode */
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd != -1)
 	{
-		buff = malloc(letters);
+		buff = malloc(sizeof(char) * letters);
 		if (!buff)
 			return (0);
 		/* read the content of the file */
 		r_byte = read(fd, buff, letters);
 		if (r_byte == -1)
+		{
+			free(buff);
 			return (0);
+		}
 
 		/* write it out to stdout */
-		byte = write(1, buff, letters);
-		if (byte == -1)
+		w_byte = write(STDOUT_FILENO, buff, r_byte);
+		if (w_byte == -1)
+		{
+			free(buff);
 			return (0);
+		}
 
 		/* close the file */
 		close(fd);
-		return (byte);
+		free(buff);
+		return (w_byte);
 	}
 	return (0);
 }
